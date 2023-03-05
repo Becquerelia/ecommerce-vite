da21b1c98e28b18a2b0b7459b56c008d2d897de6
Eva GC <evarteca@gmail.com>
2023-03-05 22:58:35 +0100

    add linter config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { API_URL } from "../env";
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>      
+      <h1>Hello world</h1>
+      <p> A: {API_URL} </p>      
     </div>
   )
 }
